8d0b9e35421593959c98dd6f2d324c3d56044a9c
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-04 16:06:45 +0530

    Add files via upload
    It's my First Python Work

---
+++ jacob.py
@@ -0,0 +1,71 @@
+'''
+print('Hello\nWorld')
+
+if 5 > 3 :
+    print('Five is greater than two')
+if 5 > 3 :
+       print('Five is grater than three')
+if 5 > 3 :
+       print('Five is greater than three')
+if 5 > 3 :
+       print('Five is greater than three')
+
+x=5
+y=3
+print(x)
+print(y)
+
+x=5
+x="thomas"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+
+x = 3j
+y = 'John'
+print(type(x))
+print(type(y))
+
+
+a = 4
+A = 'thomas'
+print(A)
+
+myavr = ("thomas")
+myavr = ("thomas")
+print(myavr)
+
+x ,y ,z = "orange","Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+x = "python "
+y = "is "
+z = "awesome "
+print(x + y+ z )
+
+username = input("Enter Username")
+print("Username is: " + username)
+
+a = "Hello "
+b = " World"
+c = a + b
+print(c)
+
+a =  "Hello, World "
+print(a[8])
+'''
+b = "Hello ,  World"
+print(b[:15])
