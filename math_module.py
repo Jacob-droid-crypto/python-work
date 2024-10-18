42f5ef71667c12ca2a2fac8a2822a9c1e8211419
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-18 16:18:06 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,15 @@
+
+'''
+Author: jacob Thomas
+Date:18/10/2024
+Description:a Python program that uses functions from the math module to perform the following operations on a number provided by the user
+'''
+import math
+number= int(input("Enter a number:"))
+square_root=math.sqrt(number)
+print("Square_root of",number,":",square_root)
+factorial=math.factorial(number)
+print("Factorial of ",number,":",factorial)
+power=math.pow(5,2)
+print(number,"raised to 2 ",":",power)
+
