1cfe8c94809847ed33e4391c8b8c49fc1e487edf
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-18 15:55:40 +0530

    Add files via upload

---
+++ integer demo.py
@@ -0,0 +1,13 @@
+'''
+Author:Jacob  Thomas
+Date:18/10/2024
+Description: A Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the aecond number:"))
+print('Sum:',number1+ number2)
+print("Division:",number1/number2)
+print("Is number1 greater than number2?:",number1>number2)
+print("Are number1 and number2 equal?:",number1==number2)
+print("Logical AND:",number1>0 and number2>0)
+print("Logical OR:",number1>0 or number2<0)
