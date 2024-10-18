63cb67e877a496667692a3de5439d0a15591eaa0
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-18 15:09:25 +0530

    Add files via upload

--- string demo.py
+++ string demo.py
@@ -1,3 +1,9 @@
+'''
+Authoy: Jacob Thomas
+Date : 18/10/24
+Description: a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+World Everyone
+'''
 str_var = "Hello, World!"
 str_another_var =str_var[7:12]
 str_new_var = (" Everyone!")
