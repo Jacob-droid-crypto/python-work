939c09b0659143efc62f9b0afc31f523f91babe4
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-18 16:19:51 +0530

    Add files via upload

--- math_module.py
+++ math_module.py
@@ -1,6 +1,6 @@
 
 '''
-Author: jacob Thomas
+Author: Jacob Thomas
 Date:18/10/2024
 Description:a Python program that uses functions from the math module to perform the following operations on a number provided by the user
 '''
@@ -12,4 +12,27 @@ factorial=math.factorial(number)
 print("Factorial of ",number,":",factorial)
 power=math.pow(5,2)
 print(number,"raised to 2 ",":",power)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
