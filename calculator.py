7118981940136584c26b050c8206d26693b637be
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-19 14:41:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+'''
+Author:Jacob Thomas
+Date : 19/10/2024
+Description:Write a Python program that performs the Simple desktop calculator functions
+'''
+number_1=int(input("Enter the first number"))
+number_2=int(input("Enter the second number"))
+number_3=int(input("Enter the third number"))
+print("Sum of number_1 and number_2 is :",number_1+number_2)
+
+print("The difference when number_2 is subtracted from number_2 is :",number_1-number_2)
+print("The product of number_1 and number_2  is :", number_1* number_2)
+print("The quotient when number_1 is divided by number_2 is:",number_1/number_2 )
+print("The remainder when number_1 is divided by number_2 is:",number_1%number_2)
+print("The result of (number_1 + number_2) * number_3 / 2 is:",(number_1 + number_2)*number_3)
