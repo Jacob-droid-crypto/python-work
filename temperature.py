049e8f9273b3238d763809e7e89f90681cef3cc0
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-25 16:08:18 +0530

    Add files via upload

--- temperature.py
+++ temperature.py
@@ -1,15 +1,16 @@
 '''
-Author
+Author:Jacob Thomas
+Date: 25/10/2024
+Description:Write a Python program to convert temperature values back and forth between Celsius and Fahrenheit. The user should be able to input a temperature in Celsius or Fahrenheit, and the program should convert it to the other unit using the formula
 '''
 
 
 temp1=int(input("Enter  the temperature"))
-scale=input("Is this in (C)elsius or (F)aranheit ")
+scale=input("Is this in (C)elsius or (F)aranheit ?")
 if scale=='C':
-    f=(9/5*temp1)+32
-    print(f)
+    f=((9/5)*temp1)+32
+
     print(temp1,"Celsius is",f)
 else:
     c=(5/9)*(temp1-32)
-    print(c)
-print(temp1,"faranheit is",c)
+    print(temp1,"faranheit is", c)
