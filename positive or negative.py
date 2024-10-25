ae5157689fbe0a1ea6647a8f692998707b07a162
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-25 14:37:40 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,16 @@
+'''
+Author:Jacob Thomas
+Date:25/10/2024
+Description: Checking whether a number is positive or negative
+'''
+
+
+number1=int(input("Enter first number"))
+if(number1>0):
+    print(number1,"is positive")
+elif(number1<0):
+    print(number1,"is negative")
+else:
+    print(number1,"is zero")
+
+
