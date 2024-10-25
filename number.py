f3a7d56e3c387ca0bcd83737683f9a166bfea68f
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-25 14:37:23 +0530

    Add files via upload

--- number.py
+++ number.py
@@ -1,7 +1,7 @@
 '''
 Author:Jacob Thomas
 Date:25/10/2024
-Description:Write a Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest.
+Description:Write a Python program to find the largest of two numbers. The program should take two numbers as input from the user and determine which one is the largest.
 '''
 number1=int(input("Enter first number"))
 number2=int(input("Enter second number"))
