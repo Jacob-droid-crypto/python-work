fcfef77755f22531d31ca112fbb6c6d0a63a2517
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-10-28 10:05:45 +0530

    Add files via upload

---
+++ atm_demo.py
@@ -0,0 +1,25 @@
+balance_amount=100001
+while True:
+    print("\n1. \tCheck Balance")
+    print("2.\tDeposit Money")
+    print("3. \tWithdraw Money")
+    print("4. \tExit" )
+    choice= int(input("Enter your choice:"))
+    if choice==1:
+        print(f"The current balance is ${balance_amount}")
+    elif choice==2:
+        deposit_amount = float(input("Enter the amount "))
+        balance_amount = balance_amount +deposit_amount
+        print(f"The deposited amount is $ {deposit_amount} and " f" the current balance amount is ${balance_amount}")
+    elif choice==3:
+        withdraw_amount = float(input("Enter the amount to withdraw"))
+        if(withdraw_amount>balance_amount):
+            print("Insufficient balance")
+        else:
+            balance_amount = balance_amount - withdraw_amount
+            print(f"The amount withdrawn is ${withdraw_amount} and " f"The balance amount is ${balance_amount}")
+
+    elif choice == 4:
+        break
+    else:
+        print("Invalid Entry")
