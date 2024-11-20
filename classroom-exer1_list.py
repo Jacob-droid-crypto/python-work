821d300e583f89f3dcd6f47e007a972f764bd65f
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-11-20 23:37:05 +0530

    Add files via upload

---
+++ classroom-exer1_list.py
@@ -0,0 +1,10 @@
+'''
+Author:Jacob Thomas
+Date:21/11/2024
+Description;: How to create lists, access elements using indexing, and modify list elements.
+'''
+fruits=["apple", "banana", "cherry","date"]
+print(fruits[2])
+fruits[3]="Dragonfruit"
+print(fruits)
+
