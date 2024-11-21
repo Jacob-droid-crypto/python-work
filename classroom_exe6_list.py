5a5b49d66228afc367633e9a584714a8180ca5fd
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-11-21 20:30:21 +0530

    Add files via upload

---
+++ classroom_exe6_list.py
@@ -0,0 +1,12 @@
+'''
+Author:Jacob thomas
+Date:21/11/2024
+Description:"Use the reverse() method to reverse the order of elements in a list.
+► Instructions:
+► Create a list of numbers: [10, 20, 30, 40, 50].
+► Reverse the order of the list.
+► Print the reversed list.
+'''
+numbers=[10, 20, 30, 40, 50]
+numbers.reverse()
+print(numbers)
