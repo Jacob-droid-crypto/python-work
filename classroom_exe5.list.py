79107e127b4b41043fffcdc9c6175c2214409386
Jacob Thomas <jacobthomassjcet@gmail.com>
2024-11-21 20:25:57 +0530

    Add files via upload

---
+++ classroom_exe5.list.py
@@ -0,0 +1,14 @@
+'''
+Author:Jacob Thomas
+Date:21/11/2024
+Description:: Use the count() method to count occurrences of a value in a list.
+► Instructions
+► Create a list: ["apple", "banana", "cherry", "apple", "apple", "banana"].
+► Count how many times "apple" appears in the list.
+► Count how many times "banana" appears in the list
+'''
+
+
+fruits=["apple", "banana", "cherry", "apple", "apple", "banana"]
+print(fruits.count("apple"))
+print(fruits.count("banana"))
